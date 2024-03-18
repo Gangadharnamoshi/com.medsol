cd7ed9274ed81886a9a0af67c6a134b6c528d3a6
Gangadharnamoshi <150498432+Gangadharnamoshi@users.noreply.github.com>
2024-03-18 16:05:49 +0530

    Final

--- src/test/java/com/pages/Baseclass.java
+++ src/test/java/com/pages/Baseclass.java
@@ -8,7 +8,6 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
